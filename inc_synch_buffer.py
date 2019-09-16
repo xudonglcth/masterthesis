4d14e69ba145364f656a9fd6c1747342b79b38cc
Xudong Liang <xudongl@student.chalmers.se>
2019-09-16 14:17:41 +0200

    updated inc_sync_buffer 2019/09/16

--- inc_synch_buffer.py
+++ inc_synch_buffer.py
@@ -1,4 +1,6 @@
 import numpy as np
+from BBS_logical import bbs_abstraction
+from BBS_logical import G
 
 
 def t_2_n_sigma(t):
@@ -53,8 +55,33 @@ def bbs(sigma, t, init, lmd, sigma_local):
     if np.size(sigma_local) != 0:
         t[np.in1d(t[:, 1], sigma_local), 1] = tau
         sigma = np.union1d(sigma, tau)
+    g = G(transition=t, initial=init, block=lmd, tau=tau)
+    n_pi, t_pi, i_pi, lmd_pi, pi, iteration = bbs_abstraction(g)
+    sigma_pi = np.setdiff1d(sigma, sigma_local)
+    return n_pi, sigma_pi, t_pi, i_pi, lmd_pi, pi, iteration
 
 
+def trans2delta(n, t, backward=False):
+    if backward:
+        i_source = 3
+        i_target = 1
+    else:
+        i_source = 1
+        i_target = 3
+
+    n_delta = np.zeros(n, int)
+    delta = np.zeros(n, int)
+    sigma_delta = np.zeros(n, int)
+
+    if np.size(t) == 0:
+        return n_delta, delta, sigma_delta
+    for i in range(len(t[:, 0])):
+        q = t[i, i_source]
+        n_delta[q - 1] = n_delta[q - 1] + 1
+        delta[n_delta[q - 1] - 1, q - 1] = t[i, i_target - 1]
+        sigma_delta[n_delta[q - 1] - 1, q - 1] = t[i, 1]
+        
+    return n_delta, delta, sigma_delta
 
 
 
@@ -65,5 +92,7 @@ def inc_sync_buffer(n_cap, n_buff, inc_bbs):
     n, sigma, t, init, lmd, sigma_local = ts_model(model, a, n_cap, mark)
     a += 1
     if inc_bbs:
-        pass
+        n, sigma, t, init, lmd, pi, iteration = bbs(sigma, t, init, lmd, sigma_local)
+
+    n_delta, delta, sigma_delta = trans2delta
 
